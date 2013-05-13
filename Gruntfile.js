49c381444decd038c9ad594857ae45ac21d27489
Kokudori <kokudori0912@gmail.com>
2013-05-13 18:16:57 +0900

    use npm and bower

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		concat: {
+			'app.js': [
+				'components/jquery/jquery.js',
+				'components/bootstrap/docs/assets/js/bootstrap.js',
+				'js/index.js'
+			],
+			'app.css': [
+				'components/bootstrap/docs/assets/css/bootstrap.css',
+				'components/bootstrap/docs/assets/css/bootstrap-responsive.css',
+				'css/index.css'
+			]
+		},
+		uglify: {
+			all: {
+				files: {
+					'app.js': ['app.js']
+				}
+			}
+		},
+		jshint: {
+			files: ['js/*.js'],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+};
